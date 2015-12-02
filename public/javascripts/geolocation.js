e880423684c3f750fac594c76144649a4510e02a
Ben <B.Seroussi@utexas.edu>
2015-12-01 21:40:17 -0600

    Geolocation is pretty much done, but needs to be extensively tested. I also changed it so that we only update the html of the groups we need to update when we switch groups

--- public/javascripts/geolocation.js
+++ public/javascripts/geolocation.js
@@ -15,6 +15,46 @@ Geo.groupWithinDistance = function(group, callback) {
   });
 };
 
-navigator.geolocation.getCurrentPosition(function (position) {
-    console.log(position);
-});
+Geo.allGroupsWithinDistanceFromPos = function(groups, position, callback) {
+  var groupsList = [];
+  groups.forEach(function(group) {
+    if(PositionFunctions.isWithinMaxDist(group.position, position)) {
+      groupsList.push(group);
+    }
+  });
+  callback(groupsList);
+};
+
+Geo.allGroupsWithinDistance = function(groups, callback) {
+  Geo.getCurrentPosition(function(position) {
+    Geo.allGroupsWithinDistanceFromPos(groups, position, callback);
+  });
+};
+
+var previousPosition = null;
+Geo.updateGroupsOnChange = function(position) {
+  if((previousPosition != null) && (previousPosition.coords.longitude == position.coords.longitude) &&
+     (previousPosition.coords.latitude == position.coords.latitude)) { return; }
+  previousPosition = position;
+  G.getAllGroupsOnServer(function(groups) {
+    Geo.allGroupsWithinDistanceFromPos(groups, position, function(groupsList) {
+      G.updateSidebar(groupsList);
+    });
+  });
+};
+
+/*
+ *
+ *
+ */
+Geo.updateGroupsOnStartup = function() {
+  Geo.getCurrentPosition(function(position) {
+    Geo.updateGroupsOnChange(position);
+  });
+}
+
+Geo.start = function() {
+  navigator.geolocation.watchPosition(Geo.updateGroupsOnChange);
+};
+
+Geo.start();
